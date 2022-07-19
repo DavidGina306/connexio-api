5c1cab8be7242efeb5d4d0ef5378c50e565fbdb8
David Felipe Santos Oliveira <davidfelipe_306@hotmail.com>
2022-07-18 22:20:42 -0300

    Corrigindo ListSearch

--- app/Services/SearchAreas/ListSearchAreasService.php
+++ app/Services/SearchAreas/ListSearchAreasService.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Services\SearchAreas;
+
+use App\Http\Resources\SearchAreaResource;
 use App\SearchArea;
 use Exception;
 use Illuminate\Support\Facades\Log;
@@ -10,8 +12,8 @@ class ListSearchAreasService
     public static function list()
     {
         try {
-            $areas = SearchArea::all()->toArray();
-            return new SearchArea($areas);
+            $areas = SearchArea::all();
+            return SearchAreaResource::collection($areas);
         } catch (Exception $e) {
             Log::error($e->getMessage());
             throw new Exception('Error to List SearchAreas', 500);
